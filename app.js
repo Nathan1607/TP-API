43ef05d441049ca1b18a4ec9790704a427833c04
Nathan Gaulard <gaulardnathan@gmail.com>
2023-06-13 17:26:35 +0200

    check database select

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const sequelize = new Sequelize('liveAddict', 'root', 'rootpwd', {
   host: 'localhost',
   dialect: 'mariadb',
   port: 4000,
+  define: {
+    freezeTableName: true
+  }
 });
 
 async function connectToDatabase() {
@@ -20,6 +23,31 @@ async function connectToDatabase() {
 }
 
 connectToDatabase();
+
+const Visiteur = sequelize.define('Visiteur', {
+  nom: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  prenom: {
+    type: Sequelize.STRING,
+    allowNull: false 
+  }
+});
+
+console.log(Visiteur === sequelize.models.User);
+
+
+app.get('/visiteur', async (req, res) => {
+  try {
+    const Visiteurs = await Visiteur.findAll();
+    res.json(Visiteurs);
+  } catch (error) {
+    console.error('Error retrieving Visiteurs:', error);
+    res.status(500).json({ error: 'Failed to retrieve Visiteurs' });
+  }
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello world !!');
